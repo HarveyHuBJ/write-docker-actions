c393c36fce6574f9c99462775243a4e0969e1e82
HarveyHuBJ <tigerush80@hotmail.com>
2022-05-26 06:48:50 +0800

    Add parameters

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+) 
 
 func main(){
 	fmt.Println("Hello , Docker Actions!")
+
+	firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
+	secondGreeting := os.Getenv("INPUT_SECONDGREETING")
+	thirdGreeting := os.Getenv("INPUT_THIRDGREETING")
+
+	fmt.Println("Hello, "+ firstGreeting )
+	fmt.Println("Hello, "+ secondGreeting )
+
+  	// Someimes inputs are not "required" and we can build around that
+  	if thirdGreeting != "" {
+    	fmt.Println("Hello " + thirdGreeting)
+    }
+
 }
